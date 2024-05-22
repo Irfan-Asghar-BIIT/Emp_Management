cef29bd50261bfaa3b4fbce9ae44d855c91e0811
Irfan Asghar <142321849+Irfan-Asghar-BIIT@users.noreply.github.com>
2024-05-22 17:54:02 +0500

    Add project files.

---
+++ Emp_Management/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Emp_Management.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+      
+        public string Name { get; set; }
+       
+        public string address { get; set; }
+      
+        public int salary { get; set; }
+    }
+}
---
+++ Emp_Management/Models/EmployeeDB.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Emp_Management.Models
+{
+    public class EmployeeDB:DbContext
+    {
+        public EmployeeDB(DbContextOptions option):base(option) 
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
